93d88be9b0235214589fa51040ce504479a5e5ad
Coolboyzlol99 <avyangupta1212@gmail.com>
2026-04-21 18:59:09 +0530

    My Daily Routine

---
+++ Mydailyroutine.py
@@ -0,0 +1,15 @@
+print("Hi! My name is Avyan and today I will be telling you about my daily routine.")
+#Step 1
+print("First, I wake up in the morning.")
+#Step 2
+print("Then I brush my teeth and take a bath.")
+#Step 3
+print("Then I wear my clothes and get ready.")
+#Step 4
+print("Then I pack my school bag according to the days time table.")
+#Step 5
+print("Then I go down and wait for the bus to arrive.")
+#Step 6
+print("Then I sit in the bus and go to school.")
+#Done
+print("That is my daily routine in the morning. Thank you!")
